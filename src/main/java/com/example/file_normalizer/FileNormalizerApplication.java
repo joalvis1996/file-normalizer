c01d4700aa777569096cda0b468de9f0ef04f10b
alvisjo <lwalwakk1@naver.com>
2025-08-13 17:23:08 +0900

    초기 설정

---
+++ src/main/java/com/example/file_normalizer/FileNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_normalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileNormalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileNormalizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/file_normalizer/FileNormalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_normalizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileNormalizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
